75c58cedc2b783a4e2e756d12c95ae001cce9758
Dana <djsilvertail@byui.edu>
2022-01-12 16:58:41 -0800

    week two team assignment

--- routes/ta02.js
+++ routes/ta02.js
@@ -1,14 +1,43 @@
-//TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
 
+const userArray = ['Dana', 'Mel', 'Steve'];
+let error = '';
+
+router.post('/addUser', (req, res, next) => {
+  const newUser = req.body.newUser;
+  const index = userArray.indexOf(newUser);
+  if (index !== -1) {
+    error = 'Person already exists. Please try again!';
+  }
+  else {
+  userArray.push(newUser);
+  error = '';
+}
+  res.redirect('/ta02/');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const remUser = req.body.remUser;
+  const index = userArray.indexOf(remUser);
+  if (index !== -1) {
+    userArray.splice(index, 1);
+    error = '';
+  } else {
+    error = 'Person not found. Please try again!';
+  }
+
+  res.redirect('/ta02/');
+});
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    users: userArray,
+    errorMessage: error,
+    path: '/ta02', 
+    activeTA03: true, 
+    contentCSS: true, 
   });
 });
 
