a7a6f0dda3db66982d9444ea58c6abe669b7d047
Dana <djsilvertail@byui.edu>
2022-01-24 08:20:28 -0800

    o

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,34 @@
 //TA03 PLACEHOLDER
+const fs = require("fs");
 const express = require('express');
+const path = require("path");
 const router = express.Router();
 
+const ItemPath = path.join(require.main.path, "data", "Items.json");
+
 router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+
+  let gotData = req.body.search;
+  console.log(gotData);
+
+  fs.readFile(ItemPath, (error, fileContent) => {
+    let itemList = [];
+
+    if(!error){
+      itemList = JSON.parse(fileContent);
+    }
+
+      res.render('pages/ta03', {
+        title: 'Team Activity 03',
+        path: '/ta03', // For pug, EJS
+        activeTA03: true, // For HBS 
+        contentCSS: true, // For HBS
+        items: itemList
+      });
   });
+
+
+  
 });
 
 module.exports = router;
